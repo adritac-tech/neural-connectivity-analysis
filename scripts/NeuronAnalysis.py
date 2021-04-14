df69d30cfd141ee598529e456542a0a6b39d90c9
adritac-tech <adrita.chakraborty@outlook.com>
2021-04-13 20:21:22 -0700

    added sample test

--- scripts/NeuronAnalysis.py
+++ scripts/NeuronAnalysis.py
@@ -9,15 +9,49 @@ from scipy import stats
 from matplotlib import colors
 from matplotlib.ticker import PercentFormatter
 
-excelData = pd.read_excel(r'/Users/Adrita1/Programs2.0/Internship/Data/CorrectNeuronConnect.xls') #this location varies for the computer
 
-# testData = pd.read_excel(r'/Users/Adrita1/Programs2.0/Internship/Data/Test.xls') #this location varies for the computer
-# #testPandas = pd.DataFrame(testData)
-# testDataNumpy = testData.to_numpy()
-# testIntensity, testCoherance, testFrequency = bct.algorithms.motif3funct_wei(testDataNumpy)
-# np.save("testIntensity.npy", testIntensity)
-# np.save("testCoherance.npy", testCoherance)
-# np.save("testFrequency.npy", testFrequency)
+excelData = pd.read_excel(r'/Users/Adrita1/Programs2.0/Internship/Data/CorrectNeuronConnect.xls') #this location varies for the computer
+#TESTING BEGINS HERE
+testData = pd.read_excel(r'/Users/Adrita1/Programs2.0/Internship/Data/Test.xls', header=None) #this location varies for the computer
+#testPandas = pd.DataFrame(testData)
+testDataNumpy = testData.to_numpy()
+
+print(testDataNumpy.shape)
+testIntensity, testCoherance, testFrequency = bct.algorithms.motif3funct_wei(testDataNumpy.T)
+np.save("testIntensity.npy", testIntensity)
+np.save("testCoherance.npy", testCoherance)
+np.save("testFrequency.npy", testFrequency)
+
+#TESTING ENDS HERE
+
+# motif1 = bct.algorithms.find_motif34(0,3)
+# print(motif1[:,:,0])
+motif1 = bct.algorithms.find_motif34(1,3)
+print(motif1[:,:,0])
+motif2 = bct.algorithms.find_motif34(2,3)
+print(motif2[:,:,0])
+motif3 = bct.algorithms.find_motif34(3,3)
+print(motif3[:,:,0])
+motif4 = bct.algorithms.find_motif34(4,3)
+print(motif4[:,:,0])
+motif5 = bct.algorithms.find_motif34(5,3)
+print(motif5[:,:,0])
+motif6 = bct.algorithms.find_motif34(6,3)
+print(motif6[:,:,0])
+motif7 = bct.algorithms.find_motif34(7,3)
+print(motif7[:,:,0])
+motif8 = bct.algorithms.find_motif34(8,3)
+print(motif8[:,:,0])
+motif9 = bct.algorithms.find_motif34(9,3)
+print(motif9[:,:,0])
+motif10 = bct.algorithms.find_motif34(10,3)
+print(motif10[:,:,0])
+motif11 = bct.algorithms.find_motif34(11,3)
+print(motif11[:,:,0])
+motif12 = bct.algorithms.find_motif34(12,3)
+print(motif12[:,:,0])
+motif13 = bct.algorithms.find_motif34(13,3)
+print(motif13[:,:,0])
 
 # #TESTING ENDS HERE
 chart = pd.DataFrame(excelData, columns = ['Neuron 1','Neuron 2', 'Type', 'Nbr'])
